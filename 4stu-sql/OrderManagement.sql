7272133325759ebb56f3d7bb9eefa3403ef2fdc7
vanhuutoan27 <vhtoan27@gmail.com>
2023-10-13 21:33:33 +0700

    update order completion, update staff page

--- 4stu-sql/OrderManagement.sql
+++ 4stu-sql/OrderManagement.sql
@@ -1,7 +1,7 @@
 CREATE TABLE OrderManagement (
     OrderId INT IDENTITY(1,1) NOT NULL,
     dateCreated DATE DEFAULT GETDATE(),
-    dateCompleted DATE,
+    dateCompleted DATE DEFAULT 'Not Yet',
     CustomerId INT NOT NULL,
 	CustomerName VARCHAR(50) NOT NULL,
     Email VARCHAR(50) NOT NULL,
@@ -11,6 +11,7 @@ CREATE TABLE OrderManagement (
     paymentMethod VARCHAR(50) NOT NULL,
     ServiceId INT NOT NULL,
 	ServiceName VARCHAR(50) NOT NULL,
+	NOTE VARCHAR(50),
     Status VARCHAR(20) DEFAULT 'Pending' NULL, -- Processing or Completed 
     StaffId INT NOT NULL,
 	PRIMARY KEY(OrderId)
